acccae5127f939858d9d9dee84fa8095c8a29894
Ken P <87345178+airplane3333@users.noreply.github.com>
2021-12-05 13:32:38 -0700

    first update

---
+++ static/js/logic.js
@@ -0,0 +1,4 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
