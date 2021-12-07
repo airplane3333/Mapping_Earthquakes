ac5a1373e8e046366d9e805c455eae2d64592c4f
Ken P <87345178+airplane3333@users.noreply.github.com>
2021-12-07 16:34:02 -0700

    update

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([45.31, -122.40], 4);
 
 // Create the map object with a center and zoom level.
 // this allows for individule paramters without setView
@@ -12,10 +12,17 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}' ,{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    id: "mapbox/dark-v10",
     accessToken: api_key
-});
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+}).addTo(map);
+
+L.circle([34.052245, -118.2437], {
+    color: 'black',
+    fillColor: '#ffffa1',
+    fillOpacity: 0.7,
+    radius: 300
+}).addTo(map);
